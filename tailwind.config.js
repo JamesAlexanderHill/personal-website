ed8be5938bcaff2080d615538b0a44bacddc9609
James Hill <info@jamesalexanderhill.com>
2022-08-01 04:19:43 +1000

    add animations + quicklinks + basic content

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,23 @@ module.exports = {
     "./src/**/*.{html,js}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      heading: ['Montserrat', 'sans-serif'],
+      body: ['Inconsolata', 'serif'],
+    },
+    extend: {
+      animation: {
+        'fade-in-up': 'fadeInUp 0.8s ease-in-out backwards',
+      },
+      keyframes: {
+        fadeInUp: {
+          '0%': { transform: 'translateY(20px)', opacity: 0 },
+          '100%': { transform: 'translateY(0px)', opacity: 100 },
+        }
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require("tailwindcss-animation-delay"),
+  ],
 }
