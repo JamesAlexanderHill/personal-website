19ebf5196a4adb1d59987d5d96db16d019b566a0
James Hill <info@jamesalexanderhill.com>
2022-08-06 17:30:38 +1000

    Add js linting

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const titles = ["Solution Architect", "Web Engineer", 'Avid Programmer', 'Full Stack Developer'];
+const titles = ['Solution Architect', 'Web Engineer', 'Avid Programmer', 'Full Stack Developer'];
 
 document.addEventListener('DOMContentLoaded', () => {
-    const randomIndex = Math.ceil(Math.random() * titles.length -1);
-    const titleContainer = document.getElementById('titleContainer');
-    titleContainer.innerText = titles[randomIndex];
+	const randomIndex = Math.ceil(Math.random() * titles.length -1);
+	const titleContainer = document.getElementById('titleContainer');
+	titleContainer.innerText = titles[randomIndex];
 });
