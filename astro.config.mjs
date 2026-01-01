3edbab9fb1032dbbd57d0261f9cfb6fec1b3b820
James Hill <info@jamesalexanderhill.com>
2026-01-01 15:15:54 +0000

    fix tailwindcss installation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [react()],
   output: "static",
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
